0acdd6716005ca66dc0b663ee0bade58c040bae2
Yegor Bugayenko <yegor@tpc2.com>
2014-11-01 22:25:04 +0100

    proto

---
+++ proto/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"/>
+    <link href="screen.css" rel="stylesheet"/>
+  </head>
+  <body>
+    <div class="body">
+      <div class="par">
+        <span><strong>curiost</strong></span>
+        <a href="">submit</a>
+        <a href="">$78.90</a>
+        <a href="">terms</a>
+        <span>@yegor256</span>
+        <a href=""><i class="fa fa-sign-out"></i></a>
+      </div>
+      <div class="par">
+        <form>
+          <fieldset>
+            <input name="q" size="30" value="name:Yegor"/>
+            <button type="submit"><i class="fa fa-search"></i></button>
+          </fieldset>
+        </form>
+      </div>
+      <?php $page = '';
+      ?>
+    </div>
+  </body>
+</html>
+
